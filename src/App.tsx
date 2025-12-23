40ec6de6c70747e93b90c8353839ab112b357d68
Adithya Shenal <washenal55@gmail.com>
2025-12-23 18:24:11 +0530

    bla bla

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
       <h2>We will learn Production grade workflow</h2>
       <h3>Docker + Docker Compose + AWS + Travis CI</h3>
       <div>Hello World</div>
+      <p>lorem 10</p>
     </>
   );
 }
